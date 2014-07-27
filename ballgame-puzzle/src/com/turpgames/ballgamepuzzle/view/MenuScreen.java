035bdfc6f2b6e759b52c3b42b2afb638527de7bc
Mehmet Ataş <mehmet@mehmetatas.net>
2014-07-27 17:40:36 +0300

    m

---
+++ ballgame-puzzle/src/com/turpgames/ballgamepuzzle/view/MenuScreen.java
@@ -0,0 +1,76 @@
+package com.turpgames.ballgamepuzzle.view;
+
+import com.turpgames.ballgamepuzzle.components.Toolbar;
+import com.turpgames.ballgamepuzzle.utils.R;
+import com.turpgames.ballgamepuzzle.utils.StatActions;
+import com.turpgames.ballgamepuzzle.utils.Textures;
+import com.turpgames.framework.v0.client.TurpClient;
+import com.turpgames.framework.v0.component.Button2;
+import com.turpgames.framework.v0.component.IButtonListener;
+import com.turpgames.framework.v0.impl.Screen;
+import com.turpgames.framework.v0.impl.ScreenManager;
+import com.turpgames.framework.v0.util.Game;
+
+public class MenuScreen extends Screen {
+	private final static float buttonWidth = 150f;
+	private final static float buttonHeight = 50f;
+
+	private Button2 playButton;
+	private Button2 aboutButton;
+
+	@Override
+	public void init() {
+		super.init();
+
+		playButton = initButton("Play", (Game.getVirtualWidth() - buttonWidth) * 0.5f, 450f, new IButtonListener() {
+			@Override
+			public void onButtonTapped() {
+				ScreenManager.instance.switchTo(R.screens.levels, false);
+			}
+		});
+
+		aboutButton = initButton("About", (Game.getVirtualWidth() - buttonWidth) * 0.5f, 350f, new IButtonListener() {
+			@Override
+			public void onButtonTapped() {
+				ScreenManager.instance.switchTo(R.screens.about, false);
+			}
+		});
+	}
+
+	@Override
+	protected void onAfterActivate() {
+		playButton.activate();
+		aboutButton.activate();
+		Toolbar.getInstance().activate();
+		Toolbar.getInstance().deactivateBackButton();
+	}
+
+	@Override
+	protected boolean onBeforeDeactivate() {
+		playButton.deactivate();
+		aboutButton.deactivate();
+		Toolbar.getInstance().deactivate();
+		return super.onBeforeDeactivate();
+	}
+
+	@Override
+	protected boolean onBack() {
+		TurpClient.sendStat(StatActions.ExitGame);
+		Game.exit();
+		return true;
+	}
+
+	private Button2 initButton(String text, float x, float y, IButtonListener listener) {
+		Button2 btn = new Button2();
+
+		btn.setText(text);
+		btn.setSize(buttonWidth, buttonHeight);
+		btn.setFontScale(0.6f);
+		btn.setListener(listener);
+		btn.setColor(R.colors.blue, R.colors.green);
+		btn.setTexture(Textures.button);
+		btn.setLocation(x, y);
+
+		return btn;
+	}
+}
